50fad07c99b50924ae02a3764977d47928da3cb0
Yash <110911562+Yashprajapati01@users.noreply.github.com>
2023-10-02 10:15:22 +0530

    Time: 1 ms (45.52%), Space: 41.1 MB (55.81%) - LeetHub

--- 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.java
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.java
@@ -2,8 +2,8 @@ class Solution {
     public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
       Arrays.sort(hours);
       int a=0;
-        for(int i=0;i<hours.length;i++){
-            if(hours[i]>=target)
+        for(int i:hours){
+            if(i>=target)
             a++;
         }
         return a;
