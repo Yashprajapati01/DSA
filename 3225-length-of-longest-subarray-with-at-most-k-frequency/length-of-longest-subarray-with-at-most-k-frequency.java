eec101c7b325982650b268585bc04df05685283d
Yash Prajapati <110911562+Yashprajapati01@users.noreply.github.com>
2024-03-28 23:37:06 +0530

    Time: 28 ms (99.44%) | Memory: 168.2 MB (5.44%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,25 +1,31 @@
 class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
-        int ans = 1;
-        int start = 0;
-        HashMap<Integer, Integer> d = new HashMap<>();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
 
-        for (int i = 0; i < nums.length; ++i) {
-            if (!d.containsKey(nums[i])) {
-                d.put(nums[i], 1);
-            } else {
-                d.put(nums[i], d.get(nums[i]) + 1);
-                if (d.get(nums[i]) > k) {
-                    while (nums[start] != nums[i]) {
-                        d.put(nums[start], d.get(nums[start]) - 1);
-                        start += 1;
-                    }
-                    start += 1;
-                    d.put(nums[i], d.get(nums[i]) - 1);
-                }
-            }
-            ans = Math.max(ans, i - start + 1);
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
         }
-        return ans;
+
+        int[] frequencies = new int[max - min + 1];
+
+        int left = 0;
+        
+        int maxWindow = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            frequencies[nums[right] - min]++;
+
+            while (frequencies[nums[right] - min] > k) {
+                frequencies[nums[left] - min]--;
+
+                left++;
+            }
+
+            maxWindow = Math.max(maxWindow, right - left + 1);
+        }
+
+        return maxWindow;
     }
 }
