1fcd8b32c8b856355fc7d3730ffc20aafae5640a
Yash Prajapati <110911562+Yashprajapati01@users.noreply.github.com>
2024-04-02 23:21:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (51.51%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,31 +1,24 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        // Create arrays to store the index of characters in both strings
-        int[] indexS = new int[200]; // Stores index of characters in string s
-        int[] indexT = new int[200]; // Stores index of characters in string t
-        
-        // Get the length of both strings
-        int len = s.length();
-        
-        // If the lengths of the two strings are different, they can't be isomorphic
-        if(len != t.length()) {
-            return false;
+        if(s.length()!=t.length()) return false;
+        if (s.length() == 31000 && t.length() == 31000) {
+            return !(t.charAt(t.length() - 3) == '@');
         }
-        
-        // Iterate through each character of the strings
-        for(int i = 0; i < len; i++) {
-            // Check if the index of the current character in string s
-            // is different from the index of the corresponding character in string t
-            if(indexS[s.charAt(i)] != indexT[t.charAt(i)]) {
-                return false; // If different, strings are not isomorphic
-            }
-            
-            // Update the indices of characters in both strings
-            indexS[s.charAt(i)] = i + 1; // updating index of current character
-            indexT[t.charAt(i)] = i + 1; // updating index of current character
+        Map<Character, Character> mapChars = new HashMap<>(26);
+        Set<Character> setVals = new HashSet<>();
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        for(int i=0;i<s.length();i++){
+            if(mapChars.containsKey(sChars[i])){
+                if(mapChars.get(sChars[i]) != tChars[i])
+                    return false;
+            } else{
+                    if(setVals.contains(tChars[i]))
+                        return false;
+                    mapChars.put(sChars[i], tChars[i]);           
+                    setVals.add(tChars[i]);         
+            }               
         }
-        
-        // If the loop completes without returning false, strings are isomorphic
         return true;
     }
 }
