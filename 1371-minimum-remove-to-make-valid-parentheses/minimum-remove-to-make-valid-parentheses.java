bc10d37437911a8572c3dc3101f979e5accfc570
Yash Prajapati <110911562+Yashprajapati01@users.noreply.github.com>
2024-04-06 23:05:41 +0530

    Time: 23 ms (46.27%) | Memory: 45.6 MB (29.12%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,24 +1,31 @@
+import java.util.Stack;
+
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Stack <Integer> st=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='(') st.push(i);
-            else if(s.charAt(i)==')'){
-                if(!st.isEmpty()&&s.charAt((int)st.peek())=='('){
-                    st.pop();
-                }
-                else st.push(i);
-            }
-            else continue;
-        }
-        StringBuilder sb=new StringBuilder();
-        for(int i=s.length()-1;i>=0;i--){
-            if(!st.isEmpty()&&(int)st.peek()==i) {
-                st.pop();
+        int n = s.length();
+        boolean[] vis = new boolean[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            if (Character.isLowerCase(s.charAt(i)))
                 continue;
+            if (s.charAt(i) == '(') {
+                st.push(i);
+            } else {
+                if (!st.isEmpty()) {
+                    st.pop();
+                } else {
+                    vis[i] = true;
+                }
             }
-            else sb.append(s.charAt(i));
         }
-        return sb.reverse().toString();
+        while (!st.isEmpty()) {
+            vis[st.pop()] = true;
+        }
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (!vis[i])
+                ans.append(s.charAt(i));
+        }
+        return ans.toString();
     }
 }
