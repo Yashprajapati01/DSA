52687861f89424dd28ee3ca5d5db245752abd92b
Yash Prajapati <110911562+Yashprajapati01@users.noreply.github.com>
2024-04-20 03:03:02 +0530

    Time: 8 ms (15.07%) | Memory: 48.9 MB (86.27%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,29 +1,45 @@
 class Solution {
     public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        
+        int numIslands = 0;
         int m = grid.length;
         int n = grid[0].length;
-        int count = 0;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j]=='1'){
-                    count++;
-                    dfs(i, j, grid);
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        
+        Queue<int[]> queue = new ArrayDeque<>();
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    numIslands++;
+                    queue.offer(new int[]{i, j});
+                    
+                    while (!queue.isEmpty()) {
+                        int[] curr = queue.poll();
+                        int x = curr[0];
+                        int y = curr[1];
+                        
+                        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != '1') {
+                            continue;
+                        }
+                        
+                        grid[x][y] = '0'; // mark as visited
+                        
+                        for (int[] dir : directions) {
+                            int nx = x + dir[0];
+                            int ny = y + dir[1];
+                            if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == '1') {
+                                queue.offer(new int[]{nx, ny});
+                            }
+                        }
+                    }
                 }
             }
         }
-        return count;
-    }
-    public void dfs(int i, int j, char[][] grid) {
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length){
-            return;
-        }
-        if(grid[i][j] != '1'){
-            return;
-        }
-        grid[i][j] = 'a';
-        dfs(i+1, j, grid);
-        dfs(i-1, j, grid);
-        dfs(i, j+1, grid);
-        dfs(i, j-1, grid);
+        
+        return numIslands;
     }
 }
