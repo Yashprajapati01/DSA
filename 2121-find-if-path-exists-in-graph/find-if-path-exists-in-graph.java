37f4836acf78339f577e11bc7559cfead4168020
Yash Prajapati <110911562+Yashprajapati01@users.noreply.github.com>
2024-04-21 23:01:49 +0530

    Time: 3 ms (99.62%) | Memory: 109.2 MB (94.00%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,51 +1,20 @@
 class Solution {
-
-    boolean flag = false;
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-
-        if(source == destination)return true;
-
-        Map<Integer,List<Integer>> map = new HashMap<>();
-
-        for(int i =0;i<n;i++)
-        {
-            map.put(i,new ArrayList());
-        }
-        for(int []edge : edges)
-        {
-            map.get(edge[0]).add(edge[1]);
-            map.get(edge[1]).add(edge[0]);
-
-        }
-
-        boolean []vis = new boolean[n];
-
-        dfs(map,vis,source,destination);
-
-
-        return flag;
-    }
-
-
-    private void dfs(Map<Integer,List<Integer>> map,boolean []vis,int source,int destination)
-    {
-        if(vis[source]||flag) return;
-
-        vis[source] = true;
-
-        for(int nei : map.get(source))
-        {
-            if(nei==destination)
-            {
+        if(edges.length == 0) return true;
+        boolean[] visited = new boolean[n];
+        boolean flag = true;
+        visited[source] = true;
+    while(flag){
+        flag = false;
+        for(int[] edge : edges){
+            if(visited[edge[0]] != visited[edge[1]]){
+                visited[edge[0]] = true;
+                visited[edge[1]] = true;
                 flag = true;
-                break;
             }
-            if(!vis[nei])
-            {
-                dfs(map,vis,nei,destination);
-            }
+            if(visited[destination]) return true;
         }
-
-
+    }
+    return false;
     }
 }
