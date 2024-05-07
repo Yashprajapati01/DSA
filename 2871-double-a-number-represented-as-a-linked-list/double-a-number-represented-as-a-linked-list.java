25e7cc2c098ce895afabb0807c4b1fdcbe88d264
Yash Prajapati <110911562+Yashprajapati01@users.noreply.github.com>
2024-05-08 03:32:00 +0530

    Time: 2 ms (100.00%) | Memory: 47.4 MB (6.81%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
@@ -1,32 +1,43 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode doubleIt(ListNode head) {
-        Stack<Integer> s = new Stack<Integer>();
-        
-        while(head!=null){
-            s.push(head.val);
-            head = head.next;
-        }
-        ListNode tail = null;
-        int res = 0;
-        while(!s.isEmpty() || res!=0){
-            tail = new ListNode(0,tail);
-            if(!s.isEmpty()){
-                res = res + s.pop() * 2;
-            }
-            tail.val = res%10;
-            res /= 10;
-        }
+        ListNode temp = head;
+        int frem = 0;
 
-        return tail;
+        if (temp != null && temp.val >= 5) {
+            frem = 1;
+        }
+        while (temp != null && temp.next != null) {
+            int value1=temp.val;
+            if (temp.next.val >= 5) {
+                int result = value1 * 2 + 1;
+                int lastDigit = result % 10;
+                temp.val = lastDigit;
+            } else {
+                int result = value1 * 2;
+                int lastDigit = result % 10;
+                temp.val = lastDigit;
+            }
+            temp = temp.next;
+        }
+        if (temp != null) {
+            temp.val = (temp.val * 2) % 10;
+        }
+        if (frem == 1) {
+            ListNode newHead = new ListNode(1);
+            newHead.next = head;
+            head = newHead;
+        }
+        return head;
+
     }
 }
