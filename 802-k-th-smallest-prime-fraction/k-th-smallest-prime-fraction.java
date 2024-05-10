f0308e16dbb4ad947f65b97a78781b2e4be33512
Yash Prajapati <110911562+Yashprajapati01@users.noreply.github.com>
2024-05-11 02:41:05 +0530

    Time: 1 ms (100.00%) | Memory: 41.9 MB (98.69%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -1,17 +1,50 @@
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        
         int n = arr.length;
-        
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((x, y) -> Double.compare((double)y[0] / y[1], (double)x[0] / x[1]));
-        
-        for (int i = 0 ; i < n ; i++) {
-            for  (int j = i+1 ; j < n ; j++) {
-                maxHeap.add(new int[] {arr[i], arr[j]});
-                if (maxHeap.size() > k) maxHeap.poll();
+
+        double low = 0;
+        double high = 1.0;
+
+        while(low<high)
+        {
+            double mid = (low+high)/2;
+            int res[] = getFractionsLessThanMid(arr, k, n, mid);
+
+            if(res[0] == k) return new int[]{arr[res[1]],arr[res[2]]};
+            else if(res[0]>k) high = mid;
+            else low = mid;
+        }
+
+        return new int[]{};
+    }
+
+    private int[] getFractionsLessThanMid(int[] arr, int k, int n, double mid)
+    {
+        double maxLessThanMid = 0.0;
+        int x = 0, y = 0;
+
+        int total = 0;
+        int j = 1;
+
+        for(int i=0;i<n-1;i++)
+        {
+            while(j<n && arr[i]>arr[j]*mid)
+            {
+                j++;
+            }
+
+            if(j==n) break;
+
+            total += (n-j);
+
+            double fraction = (double)arr[i]/arr[j];
+            if(fraction > maxLessThanMid)
+            {
+                maxLessThanMid = fraction;
+                x = i;
+                y = j;
             }
         }
-        
-        return maxHeap.peek();
+        return new int[]{total, x ,y};
     }
 }
