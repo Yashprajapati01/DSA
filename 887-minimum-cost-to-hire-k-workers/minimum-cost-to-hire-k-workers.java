7cdf6be6a4a3eec17a915d13a143348fdf7faddf
Yash Prajapati <110911562+Yashprajapati01@users.noreply.github.com>
2024-05-12 02:37:34 +0530

    Time: 24 ms (99.21%) | Memory: 45.7 MB (64.29%) - LeetSync

--- 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.java
+++ 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.java
@@ -1,30 +1,36 @@
 class Solution {
+    class WorkerRatio{
+        double ratio;
+        int quality;
+        public WorkerRatio(double r, int q){
+            ratio = r;
+            quality=q;
+        }
+    }
     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        List<Pair<Double, Integer>> ratio = new ArrayList<>();
-        int n = quality.length, qualitySum = 0;
-        double res = Double.MAX_VALUE, maxRate = 0.0;
-
-        for (int i = 0; i < n; ++i) {
-            ratio.add(new Pair<>((double) wage[i] / quality[i], i));
+        int n = quality.length;
+        WorkerRatio[] ratio = new WorkerRatio[n];
+        for(int i=0 ; i< n ;++i) ratio[i] = new WorkerRatio((double)wage[i]/quality[i],quality[i]);
+        Arrays.sort(ratio,(a,b)-> Double.compare(a.ratio,b.ratio));
+        Queue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+        int sum=0;
+        for(int i=0; i<k ; ++i){
+            pq.add(ratio[i].quality);
+            sum+=ratio[i].quality;
         }
-
-        ratio.sort(Comparator.comparingDouble(p -> p.getKey()));
-        for (int i = 0; i < k; ++i) {
-            qualitySum += quality[ratio.get(i).getValue()];
-            maxRate = Math.max(maxRate, ratio.get(i).getKey());
-            maxHeap.offer(quality[ratio.get(i).getValue()]);
+        double baseRatio = ratio[k-1].ratio;
+        double ans = baseRatio*sum;
+        for(int i=k ; i<n ; ++i){
+            double newRatio = ratio[i].ratio;
+            // get k smallest quality between 0,i
+            int cur_equality = ratio[i].quality;
+            if(pq.peek() > cur_equality){
+                sum-=pq.poll();
+                pq.add(cur_equality);
+                sum+=cur_equality;
+            }
+            ans = Math.min(ans,newRatio*sum);
         }
-
-        res = maxRate * qualitySum;
-        for (int i = k; i <n; ++i) {
-            maxRate = Math.max(maxRate, ratio.get(i).getKey());
-            qualitySum -= maxHeap.poll();
-            qualitySum += quality[ratio.get(i).getValue()];
-            maxHeap.offer(quality[ratio.get(i).getValue()]);
-            res = Math.min(res, maxRate * qualitySum);
-        }
-
-        return res;
+        return ans;
     }
 }
