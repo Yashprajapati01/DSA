1296a3d7fe94a33e8288c620b65b250544feb8b5
Yash Prajapati <110911562+Yashprajapati01@users.noreply.github.com>
2024-06-01 01:01:03 +0530

    Time: 1 ms (100.00%) | Memory: 45.4 MB (81.95%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -1,21 +1,17 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-        
-        // Build the frequency map
-        for (int num : nums) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        int ans =0 ;
+        for(int i= 0; i < nums.length;i++)
+        {
+            ans = ans ^ nums[i];
         }
-        
-        // Find the elements that appear only once
+        ans &= -ans;
         int[] result = new int[2];
-        int index = 0;
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            if (entry.getValue() == 1) {
-                result[index++] = entry.getKey();
-            }
+        for(int n: nums)
+        {
+            if((ans & n) == 0)result[0] ^= n;
+            else result[1] ^= n;
         }
-        
         return result;
     }
 }
